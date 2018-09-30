edb86de2867b2f6314d925ca6b490b20b88e3fe9
mjosephjr <mjosephjr@hotmail.com>
2018-09-29 22:13:19 -0400

    Edits completed after first round of feedback.

--- resources/scripts/index.php
+++ resources/scripts/index.php
@@ -5,17 +5,14 @@
     $from = 'From Website Contact Form';
     $to = 'mjosephjr@hotmail.com';
     $subject = 'Hello';
-    $human = $_POST['human'];
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-    if ($_POST['submit'] && $human == '4') {
+    if ($_POST['submit']) {
         if (mail ($to, $subject, $body, $from)) {
 	    echo '<p>Your message has been sent!</p>';
 	} else {
 	    echo '<p>Something went wrong, go back and try again!</p>';
 	}
-    } else if ($_POST['submit'] && $human != '4') {
-	echo '<p>You answered the anti-spam question incorrectly!</p>';
-    }
+}
 ?>
