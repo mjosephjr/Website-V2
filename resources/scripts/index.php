31aeee5e41af4018b6d75cdd04faa37bc5305b7c
mjosephjr <38817736+mjosephjr@users.noreply.github.com>
2019-05-19 19:45:49 -0400

    Update index.php

--- resources/scripts/index.php
+++ resources/scripts/index.php
@@ -9,7 +9,7 @@
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
     if ($_POST['submit']) {
-        if (mail ($to, $subject, $body, $from)) {
+        if (mail ($to, $subject, $body)) {
 	    echo '<p>Your message has been sent!</p>';
 	} else {
 	    echo '<p>Something went wrong, go back and try again!</p>';
